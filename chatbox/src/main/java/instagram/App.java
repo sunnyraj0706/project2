b1573a150d9069b17986ed9513fad4fcee051282
root <root@ip-172-31-94-75.ec2.internal>
2024-12-06 08:03:03 +0000

    project

---
+++ chatbox/src/main/java/instagram/App.java
@@ -0,0 +1,13 @@
+package instagram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi, this is sandeep bhargav" );
+    }
+}
